4aa6a38343e4c87740f719067f9ab8a1dcb8b908
songintae <songintae92@hanmail.net>
2021-08-30 22:33:43 +0900

    client wait-for-it.sh 시간 변경

--- spring-cloud-config-client/dockerfile-entrypoint.sh
+++ spring-cloud-config-client/dockerfile-entrypoint.sh
@@ -1,3 +1,3 @@
 ./wait-for-it.sh $RABBITMQ_HOST:$RABBITMQ_PORT -t 10 -- echo "rabbitmq is up"
-./wait-for-it.sh $CLOUD_CONFIG_SERVER_HOST:$CLOUD_CONFIG_SERVER_PORT -t 10 -- echo "cloud-config-server is up"
+./wait-for-it.sh $CLOUD_CONFIG_SERVER_HOST:$CLOUD_CONFIG_SERVER_PORT -t 20 -- echo "cloud-config-server is up"
 java -jar /app.jar
