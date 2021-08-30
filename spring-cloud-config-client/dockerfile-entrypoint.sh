14bf4010ac8092369be5d95f7113615d49f2d956
songintae <songintae92@hanmail.net>
2021-08-30 22:20:21 +0900

    github action에 docker image 생성 기능 추가 및 Dockerfile 변경

--- spring-cloud-config-client/dockerfile-entrypoint.sh
+++ spring-cloud-config-client/dockerfile-entrypoint.sh
@@ -1,3 +1,3 @@
 ./wait-for-it.sh $RABBITMQ_HOST:$RABBITMQ_PORT -t 10 -- echo "rabbitmq is up"
 ./wait-for-it.sh $CLOUD_CONFIG_SERVER_HOST:$CLOUD_CONFIG_SERVER_PORT -t 10 -- echo "cloud-config-server is up"
-java -cp app:app/lib/* com.example.demo.SpringCloudConfigClientApplication
+java -jar /app.jar
